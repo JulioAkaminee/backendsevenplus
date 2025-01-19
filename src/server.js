fbc2f9af42fd992d244d377f0a3b57c4648e570b
JulioAkaminee <juliooakamine@gmail.com>
2025-01-19 20:08:01 -0300

    Permitindo que a api receba requisições apenas de dominios personalizados

--- src/server.js
+++ src/server.js
@@ -15,13 +15,15 @@ const port = process.env.PORT_SERVER
 // Middleware para o corpo da requisição (JSON)
 app.use(express.json());
 
-// Middleware de CORS para permitir requisições de diferentes origens
-app.use(cors());
 
-app.use(cors({
-  origin: `http://localhost:${port}`, 
-  methods: 'GET,POST',
-}));
+const corsOptions = {
+  origin: ['https://backendsevenplus.vercel.app', `http://localhost:${port}`], // O domínio permitido
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],  // Métodos permitidos
+  allowedHeaders: ['Content-Type', 'Authorization'],  // Cabeçalhos permitidos
+};
+
+app.use(cors(corsOptions));
+
 
 app.use('/',homepageRouter)
 app.use('/api/categorias', categoriasRouter);
